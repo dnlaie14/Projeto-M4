92f56f910a182270dd9650bdf231fbb0f8331fe1
nyvea189 <marianyvea63@gmail.com>
2025-08-26 20:30:14 -0300

    parte 2 models

---
+++ models/economiaverdemolders.js
@@ -0,0 +1,9 @@
+class EconomiaVerde {
+  constructor(id, iniciativa, impacto) {
+    this.id = id;
+    this.iniciativa = iniciativa;
+    this.impacto = impacto;
+  }
+}
+
+module.exports = EconomiaVerde;
