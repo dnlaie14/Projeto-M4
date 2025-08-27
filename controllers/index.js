fff718b6200d61068cc4cdcf0725fdf30c4911c0
Barbara <b4b0livei123098@gmail.com>
2025-08-26 21:14:23 -0300

    parte 2 (index)

--- controllers/index.js
+++ controllers/index.js
@@ -6,3 +6,14 @@ const recursosHidricosRoutes = require('./routes/recursosHidricos');
 const economiaVerdeRoutes = require('./routes/economiaVerde');
 
 app.use(express.json());
+
+app.use('/recursos-hidricos', recursosHidricosRoutes);
+app.use('/economia-verde', economiaVerdeRoutes);
+
+app.get('/', (req, res) => {
+    res.send('Bem-vindo ao projeto Recurso Hídrico & Economia Verde!');
+});
+
+app.listen(port, () => {
+    console.log(Servidor rodando em http:localhost:${port});
+});
